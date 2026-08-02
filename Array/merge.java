5b6413238af94173210dda90f1a8f590f89498f2
shravangupta9026-create <shravangupta9026@gmail.com>
2026-08-02 11:04:11 +0530

    ...

---
+++ Array/merge.java
@@ -0,0 +1,9 @@
+public class merge {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2, 4, 6, 8};
+        int[] merged = new int[arr1.length + arr2.length];
+        System.out.println(merged);
+    }
+    
+}
