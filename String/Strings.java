252fa27de068a20ae9df092a565815ec1265357f
shravangupta9026-create <shravangupta9026@gmail.com>
2026-08-04 04:41:16 +0530

    Added Java String programs

---
+++ String/Strings.java
@@ -0,0 +1,71 @@
+// import java.util.*;
+
+// public class Strings {
+//     public static void main(String[] args) {
+//         char arr[] = {'a', 'b', 'c', 'd'};
+//         String str = "abcd";
+//         String str2 = new String("xyz") ;
+
+//         // String are immutable
+//         // String kabhi change nhi hoti hai kuch change karna h to fir new String bano
+//         Scanner sc = new Scanner(System.in);
+//         String name;
+//         name = sc.nextLine();
+//         System.out.println(name);
+
+//     }
+// }
+
+
+
+// public class Strings {
+
+//     public static void main(String[] args) {
+
+//         String name  = "Hello Shaan";  // string banao
+//         System.out.println(name);      // string print kro
+        
+    
+//     }
+// }
+
+
+
+
+// public class Strings {
+
+//     public static void main(String[] args) {
+        
+//         String college = "IIT Delhi";
+//         String course = "Btech CSE";
+
+//         System.out.println(college);
+//         System.out.println(course);
+//     }
+// }
+
+
+
+// differnce between char and String
+
+// char ch = 'A' ;   // one charcter
+// String name - "A"  // String
+
+
+
+public class Strings {
+
+    public static void main(String[] args) {
+        
+        String language = "English";
+        String place = "Sahjanwa";
+        String mobile = "moto g85";
+        String laptop = "Dell 5320";
+
+        System.out.println(language);
+        System.out.println(place);
+        System.out.println(mobile);
+        System.out.println(laptop);
+
+    }
+}
