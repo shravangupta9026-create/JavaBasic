16a53310fb2063b04ef32034643b9286915458c2
shravangupta9026-create <shravangupta9026@gmail.com>
2026-08-05 21:21:23 +0530

    ...

---
+++ Array/test.java
@@ -0,0 +1,18 @@
+public class test {
+ public static void addSum() {
+    int a = 5;
+    int b = 5;
+    int sum = a+b;
+    System.out.println(sum);
+ }
+    public static void main(String[] args) {
+        addSum();
+
+       
+        //System.out.println(i);
+
+    }
+
+    
+
+} 
