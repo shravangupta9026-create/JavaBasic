fa5de53979495da5e3716e5907d8be8cb54c5884
shravangupta9026-create <shravangupta9026@gmail.com>
2026-08-06 18:43:11 +0530

    ..

---
+++ String/singlewordinput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class singlewordinput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name : ");
+        String name = sc.next();
+        System.out.println("Hello" + name);
+    }
+    
+}
