786517dd288388ff9748c0e70118220163e705be
shravangupta9026-create <shravangupta9026@gmail.com>
2026-08-09 08:34:20 +0530

    ...

---
+++ Oops/Inheritance.java
@@ -0,0 +1,24 @@
+public class Inheritance {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.eat();
+        d.bark();
+    }
+}
+
+class Animal {
+
+    void eat() {
+        System.out.println("Eating");
+    }
+}
+
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println("Barking");
+    }
+}
