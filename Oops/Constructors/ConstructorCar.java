adc752d60e6cdab97d1578f950b2135a5b05a692
shravangupta9026-create <shravangupta9026@gmail.com>
2026-09-08 00:13:02 +0530

    constructor practice

---
+++ Oops/Constructors/ConstructorCar.java
@@ -0,0 +1,36 @@
+package Constructors;
+
+public class ConstructorCar {
+    public static void main(String[] args) {
+        Car c1 = new Car("Defender", "Black ", "kmp " + 112 , 10000000, 90.5  );
+        System.out.println(c1.carname);
+        System.out.println(c1.colour);
+        System.out.println(c1.speed);
+        System.out.println(c1.carprice);
+        System.out.println(c1.capacity);
+       
+    }
+    
+}
+
+class Car {
+    String carname;
+    String colour;
+    String  speed;
+    int carprice;
+    double capacity;
+
+    Car (){
+
+
+}
+
+Car (String cn, String cl, String sp, int cp, double cap){
+carname = cn;
+colour = cl;
+speed = sp;
+carprice = cp;
+capacity = cap;
+
+}
+}
